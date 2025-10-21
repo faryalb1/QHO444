7efaf1063bab9375b20e0f80d080faf8556abbd5
faryalb1 <faryal.b1@gmail.com>
2025-10-21 19:51:52 +0100

    multi line code example

---
+++ banner_ascii.py
@@ -0,0 +1,5 @@
+text = input("Enter a word for your banner: ").strip()
+width = len(text) + 4
+print("*" * width)
+print(f"* {text} *")
+print("*" * width)
---
+++ multi_line_message.py
@@ -0,0 +1,4 @@
+print("welcome to the class of Programming")
+print("I hope you all are well")
+print("enjoy your class")
+print("this is the output example of the codes")
