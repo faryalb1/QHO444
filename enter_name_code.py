a3c69d392834ee6e3ac2eb3b318d0b1c2bf5a411
faryalb1 <faryal.b1@gmail.com>
2025-10-21 21:03:34 +0100

    robot draw code

---
+++ enter_name_code.py
@@ -0,0 +1,8 @@
+text = input("Enter a word for your banner:").strip()
+valentine = input("Enter a word for your banner:").strip()
+width=len(text) +6
+print("*" * width)
+print(f"*  {text}  *")
+print(f"*  {valentine}  *")
+print("*" * width)
+
--- name_enter.py
+++
@@ -1,3 +0,0 @@
-# Ask for a name and reply
-name = input("What is your name? ")
-print(f"Nice to meet you, {name}.")
